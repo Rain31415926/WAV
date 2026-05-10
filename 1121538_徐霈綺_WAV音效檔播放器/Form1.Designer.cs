603c45687224f627e35589621f86aeab2788ff77
zero314 <zero3.14159265358979323846@gmail.com>
2026-05-11 06:13:57 +0800

    介面美化：按鈕圖示、字型、資源檔與視窗優化
    本次更新優化 WAV 播放器 UI，包括：
    - 按鈕與標籤加入圖示、調整字型、尺寸與位置，提升美觀與易用性
    - 標籤支援自動省略過長檔名
    - 固定視窗大小、禁止最大化、視窗置中
    - 新增 Form1.resx 資源檔，為多語系與資源管理做準備

--- 1121538_徐霈綺_WAV音效檔播放器/Form1.Designer.cs
+++ 1121538_徐霈綺_WAV音效檔播放器/Form1.Designer.cs
@@ -39,78 +39,92 @@
             // 
             // btnSelectFile
             // 
-            this.btnSelectFile.Location = new System.Drawing.Point(30, 30);
+            this.btnSelectFile.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSelectFile.Location = new System.Drawing.Point(40, 31);
+            this.btnSelectFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelectFile.Name = "btnSelectFile";
-            this.btnSelectFile.Size = new System.Drawing.Size(100, 30);
+            this.btnSelectFile.Size = new System.Drawing.Size(147, 44);
             this.btnSelectFile.TabIndex = 0;
-            this.btnSelectFile.Text = "選擇檔案";
+            this.btnSelectFile.Text = "📁 選擇檔案";
             this.btnSelectFile.UseVisualStyleBackColor = true;
             this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
             // 
             // lblFileName
             // 
-            this.lblFileName.AutoSize = true;
-            this.lblFileName.Location = new System.Drawing.Point(150, 38);
+            this.lblFileName.AutoEllipsis = true;
+            this.lblFileName.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblFileName.Location = new System.Drawing.Point(200, 42);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(77, 12);
+            this.lblFileName.Size = new System.Drawing.Size(347, 25);
             this.lblFileName.TabIndex = 1;
             this.lblFileName.Text = "尚未選擇檔案";
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(30, 80);
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.Location = new System.Drawing.Point(40, 100);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(100, 30);
+            this.btnPlay.Size = new System.Drawing.Size(147, 50);
             this.btnPlay.TabIndex = 2;
-            this.btnPlay.Text = "播放";
+            this.btnPlay.Text = "▶ 播放";
             this.btnPlay.UseVisualStyleBackColor = true;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // btnPause
             // 
-            this.btnPause.Location = new System.Drawing.Point(150, 80);
+            this.btnPause.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.Location = new System.Drawing.Point(207, 100);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(100, 30);
+            this.btnPause.Size = new System.Drawing.Size(147, 50);
             this.btnPause.TabIndex = 4;
-            this.btnPause.Text = "暫停";
+            this.btnPause.Text = "⏸ 暫停";
             this.btnPause.UseVisualStyleBackColor = true;
             this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(270, 80);
+            this.btnStop.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(373, 100);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(100, 30);
+            this.btnStop.Size = new System.Drawing.Size(147, 50);
             this.btnStop.TabIndex = 3;
-            this.btnStop.Text = "停止";
+            this.btnStop.Text = "⏹ 停止";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // btnVolumeUp
             // 
-            this.btnVolumeUp.Location = new System.Drawing.Point(30, 130);
+            this.btnVolumeUp.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnVolumeUp.Location = new System.Drawing.Point(40, 169);
+            this.btnVolumeUp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVolumeUp.Name = "btnVolumeUp";
-            this.btnVolumeUp.Size = new System.Drawing.Size(100, 30);
+            this.btnVolumeUp.Size = new System.Drawing.Size(147, 50);
             this.btnVolumeUp.TabIndex = 5;
-            this.btnVolumeUp.Text = "大聲 (+)";
+            this.btnVolumeUp.Text = "🔊 音量 (+)";
             this.btnVolumeUp.UseVisualStyleBackColor = true;
             this.btnVolumeUp.Click += new System.EventHandler(this.btnVolumeUp_Click);
             // 
             // btnVolumeDown
             // 
-            this.btnVolumeDown.Location = new System.Drawing.Point(150, 130);
+            this.btnVolumeDown.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnVolumeDown.Location = new System.Drawing.Point(373, 169);
+            this.btnVolumeDown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVolumeDown.Name = "btnVolumeDown";
-            this.btnVolumeDown.Size = new System.Drawing.Size(100, 30);
+            this.btnVolumeDown.Size = new System.Drawing.Size(147, 50);
             this.btnVolumeDown.TabIndex = 6;
-            this.btnVolumeDown.Text = "小聲 (-)";
+            this.btnVolumeDown.Text = "🔉 音量 (-)";
             this.btnVolumeDown.UseVisualStyleBackColor = true;
             this.btnVolumeDown.Click += new System.EventHandler(this.btnVolumeDown_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 200);
+            this.ClientSize = new System.Drawing.Size(573, 262);
             this.Controls.Add(this.btnVolumeDown);
             this.Controls.Add(this.btnVolumeUp);
             this.Controls.Add(this.btnStop);
@@ -118,10 +132,14 @@
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.lblFileName);
             this.Controls.Add(this.btnSelectFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WAV 音效檔播放器";
             this.ResumeLayout(false);
-            this.PerformLayout();
+
         }
 
         #endregion
