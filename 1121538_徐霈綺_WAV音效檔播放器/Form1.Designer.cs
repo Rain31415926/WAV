301ccca967a73e9c1bde1a95db3cd5c157341cba
zero314 <zero3.14159265358979323846@gmail.com>
2026-05-11 05:59:57 +0800

    新增 WAV 音效檔播放器 UI 與播放功能
    新增選擇檔案、播放與停止按鈕，並顯示檔案名稱。整合 SoundPlayer 實現 WAV 檔案的播放與停止，並加入錯誤處理與使用者提示。調整表單大小與標題，提升使用體驗。

--- 1121538_徐霈綺_WAV音效檔播放器/Form1.Designer.cs
+++ 1121538_徐霈綺_WAV音效檔播放器/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(30, 30);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(100, 30);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "選擇檔案";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(150, 38);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(77, 12);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "尚未選擇檔案";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(30, 80);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(100, 30);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(150, 80);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 30);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(500, 150);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "WAV 音效檔播放器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- 1121538_徐霈綺_WAV音效檔播放器/Form1.cs
+++ 1121538_徐霈綺_WAV音效檔播放器/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace _1121538_徐霈綺_WAV音效檔播放器
 {
     public partial class Form1 : Form
     {
+        private SoundPlayer player;
+        private string wavFilePath = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "WAV 檔案 (*.wav)|*.wav|所有檔案 (*.*)|*.*";
+                openFileDialog.Title = "選擇 WAV 音效檔";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    wavFilePath = openFileDialog.FileName;
+                    lblFileName.Text = wavFilePath;
+                }
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(wavFilePath))
+            {
+                try
+                {
+                    player = new SoundPlayer(wavFilePath);
+                    player.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("播放失敗: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("請先選擇 WAV 檔案。");
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (player != null)
+            {
+                player.Stop();
+            }
         }
     }
 }
